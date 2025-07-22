d7a0009749cc33d8c9d434a5b00c8cf52ee331a7
kilicalparslan <kilicalparslan1998@gmail.com>
2025-07-22 21:16:18 +0300

    rider-coordinates schema added

---
+++ apps/logging/src/rider-coordinates/schema/rider-coordinates.schema.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type RiderCoordinateDocument = HydratedDocument<RiderCoordinate>;
+
+@Schema()
+class RiderCoordinate {
+  @Prop({ required: true })
+  latitude: number;
+
+  @Prop({ required: true })
+  longitude: number;
+
+  @Prop({ required: true })
+  rider: string;
+}
+
+export const RiderCoordinateSchema =
+  SchemaFactory.createForClass(RiderCoordinate);
